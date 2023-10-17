9bb8585eb6328d1813fb157837a4b8558dcb4c5c
Quinten Konyn <qkonyn@gmail.com>
2023-10-17 14:07:29 -0700

    Add day of the week to daily log entry titles

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,6 @@ const { DateTime } = require("luxon");
 module.exports = function(eleventyConfig) {
   // Format dates to be shorter and more human-readable
   eleventyConfig.addFilter("postDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_HUGE);
   });
 };
