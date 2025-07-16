1e177b478d0274fedafdaae558a9957a41ad48f0
Quinten Konyn <qkonyn@gmail.com>
2025-07-16 00:12:15 -0700

    Simplified anchor links
    Only works for dark mode so far. Chose to hide link indicators entirely.
    May decide to switch that to a low-contrast pound sign for
    discoverability. Tab and hover restore indicators.
    
    Lifted the CSS for pound sign display from [HTTP Archive
    Almanac](https://almanac.httparchive.org/en/2024/accessibility#introduction).
    
    Helpful [blog post about accessible anchor
    links](https://amberwilson.co.uk/blog/are-your-anchor-links-accessible/#another-option)
    that helped me decide what to do. Has great links to further discussion.

--- .eleventy.js
+++ .eleventy.js
@@ -20,27 +20,7 @@ const markdownItAnchorOptions = {
       remove: /["]/g,
     }),
   tabIndex: false,
-  permalink(slug, opts, state, idx) {
-    state.tokens.splice(
-      idx,
-      0,
-      Object.assign(new state.Token("div_open", "div", 1), {
-        // Add class "header-wrapper [h1 or h2 or h3]"
-        attrs: [["class", `heading-wrapper ${state.tokens[idx].tag}`]],
-        block: true,
-      })
-    );
-
-    state.tokens.splice(
-      idx + 4,
-      0,
-      Object.assign(new state.Token("div_close", "div", -1), {
-        block: true,
-      })
-    );
-
-    linkAfterHeader(slug, opts, state, idx + 1);
-  },
+  permalink: markdownItAnchor.permalink.headerLink(),
 };
 
 let markdownLibrary = markdownIt({
